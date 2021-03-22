20a8e71780f39b2aca43c0ec92c5a42ffc7cbf1a
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-22 17:51:16 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(0)
+  
+  let tab =['a','b','c','d']
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +16,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello react</p>
+        <p>count</p>
+        <p>{counter}</p>
+        <button onClick={() => setCounter(counter + 1)}>click me icrease</button>
+        <button onClick={() => setCounter(counter - 1)}>click me decrease</button>
+        <p>alphabet</p>
+        <p>{tab[counter2]}</p>
+        <p>{counter2}</p>
+        <button onClick={() => tab[setCounter2((counter2 + 1)%4)]}>click me icrease</button>
         <a
           className="App-link"
           href="https://reactjs.org"
